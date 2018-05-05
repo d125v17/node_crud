6363b75a3f64c8b5091b518aebfa215c606b17f8
Vladimir <dvn125@gmail.com>
2018-05-05 08:36:07 +0300

    add both parsers

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const users = require('./routes/users');
 const pages = require('./routes/pages');
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.engine('hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.use(express.static('./views/public'));
