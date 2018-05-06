f103794d6dfcfafc4fb4b199e64d99693b4988d7
Vladimir <dvn125@gmail.com>
2018-05-06 15:37:19 +0300

    apply jwt token

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const HttpError = require('http-errors');
 const logger = require('./log/logger');
 const session = require('express-session');
 const cookeParser =  require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 app.use(cookeParser());
 app.use(session({
@@ -27,6 +28,24 @@ app.set('view engine', 'hbs');
 app.use(express.static('./views/public'));
 app.use('/', pages);
 app.use('/users', users);
+
+app.get('/settoken', function(req, res){
+    var token = jwt.sign({ token: 'new' }, 'sekret');
+    res.json({
+        jwt : token
+    });
+});
+
+app.get('/testjwt', function(req, res){
+    var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6Im5ldyIsImlhdCI6MTUyNTYwOTYzNn0.lQQPHnouUbXz_S1nxawtcWHiVPlXn8H1erjxw3QJaUw';
+    jwt.verify(token, 'sekret', function(err, decoded) {
+        res.json({
+            decoded : decoded,
+            error : err
+        });
+
+    });
+});
 
 app.get('*', function(req, res){
     res.render('page404', {layout: false, title: 'cabinet'});
